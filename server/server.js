b94a0465d3b52a60f78e3713479c99fdbfdd6906
Breno-G <breno.goncalves@hotmail.com.br>
2025-09-23 18:59:51 -0300

    configurações preliminares do mongobd

---
+++ server/server.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+const {MongoClient} = require('mongodb');
+
+async function main() {
+    const uri = process.env.MONGO_URI;
+
+    const client = new MongoClient(uri);
+
+    try{
+        await client.connect();
+
+        await listDatabases(client);
+    } catch (e) {
+        console.error('Erro de conexão', e);
+    } finally {
+        await client.close();
+    }
+}
+
+main().catch(console.error)
+
+async function listDatabases(client){
+    const databasesList =  await client.db().admin().listDatabases;
+
+    console.log("Databases: ");
+    databasesList.databases.forEach(db => {
+        console.log(`- $ {db.name}`);  
+    })
+}
